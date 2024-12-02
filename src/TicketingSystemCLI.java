0cb984b92e1b1b0d5859620347f87d70f9f3d790
Inuka Silva <inukasilva85@gmail.com>
2024-12-02 13:59:05 +0530

    Completed CLI code

--- src/TicketingSystemCLI.java
+++ src/TicketingSystemCLI.java
@@ -17,7 +17,7 @@ public class TicketingSystemCLI {
                 config.printConfig();
             }
         } else {
-            config.createNewConfig(scanner);
+            config = createNewConfig(scanner);
         }
         config.saveConfiguration();
 
@@ -29,8 +29,53 @@ public class TicketingSystemCLI {
             String cmd = scanner.nextLine();
 
             if ("start".equalsIgnoreCase(cmd)) {
+            Thread vendorThread = new Thread(new Vendor(ticketpool, config.getTicketReleaseRate()));
+            Thread customerThread = new Thread(new Customer(ticketpool, config.getCustomerBuyRate()));
 
+            vendorThread.start();
+            customerThread.start();
+
+            Logger.log("Ticketing System initiated!");
+            System.out.println("Ticketing system started! Type 'stop' to terminate.");
+            
+            } else if ("stop".equalsIgnoreCase(cmd)) {
+                Logger.log("Ticketing System stopped!");
+                break;
+            } else {
+                System.out.println("Invalid command. Please enter 'start' or 'stop'.");
+            }
+        }
+
+        Logger.close();
+        scanner.close();
+    }
+
+    private static Configuration createNewConfig(Scanner scanner) {
+        System.out.print("Enter Total Tickets : ");
+        int totalTickets = validateInput(scanner);
+
+        System.out.print("Enter ticket release rate (seconds) : ");
+        int ticketReleaseRate = validateInput(scanner);
+
+        System.out.print("Enter customer buy rate (seconds) : ");
+        int customerBuyRate = validateInput(scanner);
+
+        System.out.print("Enter max ticket capacity : ");
+        int maxCapacity = validateInput(scanner);
+
+        return new Configuration(totalTickets, ticketReleaseRate, customerBuyRate, maxCapacity);
+    }
+
+    private static int validateInput(Scanner scanner) {
+        while (true) {
+            try {
+                int Input = Integer.parseInt(scanner.nextLine());
+                if (Input > 0) return Input;
+                System.out.print("Invalid input. Enter a positive integer: ");
+            } catch (NumberFormatException e) {
+                System.out.print("Invalid input. Enter a positive integer: ");
             }
         }
     }
+
 }
